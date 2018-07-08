42ac19023fde14425c04cf03a9c9438b5dbbd603
Akshay Tambekar <akshaytambekar17@gmail.com>
2018-07-08 12:56:28 +0530

    updation in blogs frontend

--- application/views/frontend/blog-details.php
+++ application/views/frontend/blog-details.php
@@ -1,98 +1,80 @@
 <div role="main" class="main" style="margin-top: 100px;">
 
-				<section class="page-header">
-					<div class="container">
-						<div class="row">
-							<div class="col-md-12">
-								<ul class="breadcrumb">
-									<li><a href="<?php echo base_url();?>home">Home</a></li>
-									<li class="active">Blog Details</li>
-								</ul>
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-md-12">
-								<h1>Blog Post</h1>
-							</div>
-						</div>
-					</div>
-				</section>
+    <section class="page-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <ul class="breadcrumb">
+                        <li><a href="<?php echo base_url(); ?>home">Home</a></li>
+                        <li><a href="<?php echo base_url(); ?>blogs">Blogs</a></li>
+                        <li class="active"><?= $blog_detail['name']?></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Blog Post</h1>
+                </div>
+            </div>
+        </div>
+    </section>
 
-				<div class="container">
+    <div class="container">
 
-					<div class="row">
-						<div class="col-md-9">
-							<div class="blog-posts single-post">
+        <div class="row">
+            <div class="col-md-9">
+                <div class="blog-posts single-post">
 
-								<article class="post post-large blog-single-post">
-									<div class="post-image">
-										<div class="owl-carousel owl-theme" data-plugin-options='{"items":1}'>
-											<div>
-												<div class="img-thumbnail">
-													<img class="img-responsive" src="<?php echo base_url();?>assets/frontend/img/blog/blog-image-1.jpg" alt="">
-												</div>
-											</div>
-											<div>
-												<div class="img-thumbnail">
-													<img class="img-responsive" src="<?php echo base_url();?>assets/frontend/img/blog/blog-image-2.jpg" alt="">
-												</div>
-											</div>
-										</div>
-									</div>
+                    <article class="post post-large blog-single-post">
+                        <div class="post-image">
+                            <div class="owl-carousel owl-theme" data-plugin-options='{"items":1}'>
+                                <div>
+                                    <div class="img-thumbnail">
+                                        <img class="img-responsive" src="<?php echo base_url(); ?>assets/images/blogs/<?= $blog_detail['images']?>" alt="">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
-									<div class="post-date">
-										<span class="day">10</span>
-										<span class="month">Jan</span>
-									</div>
+                        <div class="post-date">
+                            <?php $timestamp = strtotime($blog_detail['created_at']); ?>
+                            <span class="day"><?= date("d", $timestamp);?></span>
+                            <span class="month"><?= date("M", $timestamp);?></span>
+                        </div>
 
-									<div class="post-content">
+                        <div class="post-content">
 
-										<h2><a href="blog-post.html">Class aptent taciti sociosqu ad litora torquent</a></h2>
+                            <h2><a href="javascript:void(0)"><?= $blog_detail['name']?></a></h2>
 
-										<div class="post-meta">
-											<span><i class="fa fa-user"></i> By <a href="#">John Doe</a> </span>
-											<span><i class="fa fa-tag"></i> <a href="#">Duis</a>, <a href="#">News</a> </span>
-											<span><i class="fa fa-comments"></i> <a href="#">12 Comments</a></span>
-										</div>
+                            <div class="post-meta">
+                                <span><i class="fa fa-user"></i> By <a href="#"><?= $blog_detail['author']?></a> </span>
+<!--                                <span><i class="fa fa-tag"></i> <a href="#">Duis</a>, <a href="#">News</a> </span>-->
+                                <span><i class="fa fa-comments"></i> <a href="#">12 Comments</a></span>
+                            </div>
+                            <p><?= $blog_detail['description']?></p>
+                            
+                        </div>
+                    </article>
 
-										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lectus lacus, rutrum sit amet placerat et, bibendum nec mauris. Duis molestie, purus eget placerat viverra, nisi odio gravida sapien, congue tincidunt nisl ante nec tellus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sagittis, massa fringilla consequat blandit, mauris ligula porta nisi, non tristique enim sapien vel nisl. Suspendisse vestibulum lobortis dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Praesent nec tempus nibh. Donec mollis commodo metus et fringilla. Etiam venenatis, diam id adipiscing convallis, nisi eros lobortis tellus, feugiat adipiscing ante ante sit amet dolor. Vestibulum vehicula scelerisque facilisis. Sed faucibus placerat bibendum. Maecenas sollicitudin commodo justo, quis hendrerit leo consequat ac. Proin sit amet risus sapien, eget interdum dui. Proin justo sapien, varius sit amet hendrerit id, egestas quis mauris.</p>
-										<p>Ut ac elit non mi pharetra dictum nec quis nibh. Pellentesque ut fringilla elit. Aliquam non ipsum id leo eleifend sagittis id a lorem. Sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam massa mauris, viverra et rhoncus a, feugiat ut sem. Quisque ultricies diam tempus quam molestie vitae sodales dolor sagittis. Praesent commodo sodales purus. Maecenas scelerisque ligula vitae leo adipiscing a facilisis nisl ullamcorper. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
-										<p>Curabitur non erat quam, id volutpat leo. Nullam pretium gravida urna et interdum. Suspendisse in dui tellus. Cras luctus nisl vel risus adipiscing aliquet. Phasellus convallis lorem dui. Quisque hendrerit, lectus ut accumsan gravida, leo tellus porttitor mi, ac mattis eros nunc vel enim. Nulla facilisi. Nam non nulla sed nibh sodales auctor eget non augue. Pellentesque sollicitudin consectetur mauris, eu mattis mi dictum ac. Etiam et sapien eu nisl dapibus fermentum et nec tortor.</p>
-										<p>Curabitur nec nulla lectus, non hendrerit lorem. Quisque lorem risus, porttitor eget fringilla non, vehicula sed tortor. Proin enim quam, vulputate at lobortis quis, condimentum at justo. Phasellus nec nisi justo. Ut luctus sagittis nulla at dapibus. Aliquam ullamcorper commodo elit, quis ornare eros consectetur a. Curabitur nulla dui, fermentum sed dapibus at, adipiscing eget nisi. Aenean iaculis vehicula imperdiet. Donec suscipit leo sed metus vestibulum pulvinar. Phasellus bibendum magna nec tellus fringilla faucibus. Phasellus mollis scelerisque volutpat. Ut sed molestie turpis. Phasellus ultrices suscipit tellus, ac vehicula ligula condimentum et.</p>
-										<p>Aenean metus nibh, molestie at consectetur nec, molestie sed nulla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nec euismod urna. Donec gravida pharetra ipsum, non volutpat ipsum sagittis a. Phasellus ut convallis ipsum. Sed nec dui orci, nec hendrerit massa. Curabitur at risus suscipit massa varius accumsan. Proin eu nisi id velit ultrices viverra nec condimentum magna. Ut porta orci quis nulla aliquam at dictum mi viverra. Maecenas ultricies elit in tortor scelerisque facilisis. Mauris vehicula porttitor lacus, vel pretium est semper non. Ut accumsan rhoncus metus non pharetra. Quisque luctus blandit nisi, id tempus tellus pulvinar eu. Nam ornare laoreet mi a molestie. Donec sodales scelerisque congue. </p>
+                </div>
+            </div>
 
+            <div class="col-md-3">
+                <aside class="sidebar">
+                    <h4 class="heading-primary">Quick Links</h4>
+                    <ul class="nav nav-list mb-xlg">
+                        <li><a href="<?php echo base_url(); ?>home">Home</a></li>
+                        <li><a href="<?php echo base_url(); ?>about-us">About Us</a></li>
+                        <li><a href="<?php echo base_url(); ?>services">Services</a></li>
+                        <li><a href="<?php echo base_url(); ?>pricing">Pricing</a></li>
+                        <li><a href="<?php echo base_url(); ?>blogs">Blogs</a></li>
+                        <li><a href="<?php echo base_url(); ?>contact-us">Contact Us</a></li>
+                    </ul>
 
-										
+                </aside>
+            </div>
+        </div>
 
-										
+    </div>
 
-										
-
-									</div>
-								</article>
-
-							</div>
-						</div>
-
-						<div class="col-md-3">
-							<aside class="sidebar">
-							
-								
-							
-								<h4 class="heading-primary">Quick Links</h4>
-								<ul class="nav nav-list mb-xlg">
-									<li><a href="<?php echo base_url();?>home">Home</a></li>
-									<li><a href="<?php echo base_url();?>about-us">About Us</a></li>
-									<li><a href="<?php echo base_url();?>services">Services</a></li>
-									<li><a href="<?php echo base_url();?>pricing">Pricing</a></li>
-									<li><a href="<?php echo base_url();?>blogs">Blogs</a></li>
-									<li><a href="<?php echo base_url();?>contact-us">Contact Us</a></li>
-								</ul>
-							
-							</aside>
-						</div>
-					</div>
-
-				</div>
-
-			</div>
+</div>
--- application/views/frontend/blogs.php
+++ application/views/frontend/blogs.php
@@ -1,155 +1,158 @@
 <div role="main" class="main" style="margin-top: 100px;">
 
-				<section class="page-header">
-					<div class="container">
-						<div class="row">
-							<div class="col-md-12">
-								<ul class="breadcrumb">
-									<li><a href="<?php echo base_url();?>home">Home</a></li>
-									<li class="active">Blog</li>
-								</ul>
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-md-12">
-								<h1>Blogs</h1>
-							</div>
-						</div>
-					</div>
-				</section>
+    <section class="page-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <ul class="breadcrumb">
+                        <li><a href="<?php echo base_url(); ?>home">Home</a></li>
+                        <li class="active">Blog</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Blogs</h1>
+                </div>
+            </div>
+        </div>
+    </section>
 
-				<div class="container">
+    <div class="container">
 
-					<div class="row">
-						<div class="col-md-12">
-							<div class="blog-posts">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="blog-posts">
+                    <?php foreach($blog_details as $blogs){ ?>
+                        <article class="post post-large">
+                            <div class="post-image">
+                                <div class="owl-carousel owl-theme" data-plugin-options='{"items":1}'>
+                                    <div>
+                                        <div class="img-thumbnail">
+                                            <img class="img-responsive" src="<?php echo base_url(); ?>assets/images/blogs/<?= $blogs['images']?>" alt="">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
 
-								<article class="post post-large">
-									<div class="post-image">
-										<div class="owl-carousel owl-theme" data-plugin-options='{"items":1}'>
-											<div>
-												<div class="img-thumbnail">
-													<img class="img-responsive" src="<?php echo base_url();?>assets/frontend/img/blog/blog-image-1.jpg" alt="">
-												</div>
-											</div>
-											<div>
-												<div class="img-thumbnail">
-													<img class="img-responsive" src="<?php echo base_url();?>assets/frontend/img/blog/blog-image-2.jpg" alt="">
-												</div>
-											</div>
-											<div>
-												<div class="img-thumbnail">
-													<img class="img-responsive" src="<?php echo base_url();?>assets/frontend/img/blog/blog-image-3.jpg" alt="">
-												</div>
-											</div>
-										</div>
-									</div>
+                            <div class="post-date">
+                                <?php $timestamp = strtotime($blogs['created_at']); ?>
+                                <span class="day"><?= date("d", $timestamp);?></span>
+                                <span class="month"><?= date("M", $timestamp);?></span>
+                            </div>
 
-									<div class="post-date">
-										<span class="day">10</span>
-										<span class="month">Jan</span>
-									</div>
+                            <div class="post-content">
 
-									<div class="post-content">
+                                <h2><a href="<?php echo base_url(); ?>blog-details?id=<?=$blogs['id']?>"><?=$blogs['name']?></a></h2>
+                                <p>
+                                    <?php 
+                                        $string = strip_tags($blogs['description']);
+                                        if (strlen($string) > 200) {
+                                            $stringCut = substr($string, 0, 200);
+                                            $endPoint = strrpos($stringCut, ' ');
+                                            $string = $endPoint? substr($stringCut, 0, $endPoint):substr($stringCut, 0);
+                                            $string .= '[...]';
+                                        }
+                                        echo $string;
+                                    ?>
+                                </p>
 
-										<h2><a href="blog-post.html">Class aptent taciti sociosqu ad litora torquent</a></h2>
-										<p>Euismod atras vulputate iltricies etri elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla nunc dui, tristique in semper vel, congue sed ligula. Nam dolor ligula, faucibus id sodales in, auctor fringilla libero. Pellentesque pellentesque tempor tellus eget hendrerit. Morbi id aliquam ligula. Aliquam id dui sem. Proin rhoncus consequat nisl, eu ornare mauris tincidunt vitae. [...]</p>
+                                <div class="post-meta">
+                                    <span><i class="fa fa-user"></i> By <a href="#"><?= $blogs['author']?></a> </span>
+<!--                                    <span><i class="fa fa-tag"></i> <a href="#">Duis</a>, <a href="#">News</a> </span>-->
+                                    <span><i class="fa fa-comments"></i> <a href="#">12 Comments</a></span>
+                                    <a href="<?php echo base_url(); ?>blog-details?id=<?=$blogs['id']?>" class="btn btn-xs btn-primary pull-right">Read more...</a>
+                                </div>
 
-										<div class="post-meta">
-											<span><i class="fa fa-user"></i> By <a href="#">John Doe</a> </span>
-											<span><i class="fa fa-tag"></i> <a href="#">Duis</a>, <a href="#">News</a> </span>
-											<span><i class="fa fa-comments"></i> <a href="#">12 Comments</a></span>
-											<a href="<?php echo base_url();?>blog-details" class="btn btn-xs btn-primary pull-right">Read more...</a>
-										</div>
+                            </div>
+                        </article>
+                    <?php } ?>
 
-									</div>
-								</article>
+<!--                    <article class="post post-large">
+                        <div class="post-image">
+                            <div class="owl-carousel owl-theme" data-plugin-options='{"items":1}'>
+                                <div>
+                                    <div class="img-thumbnail">
+                                        <img class="img-responsive" src="<?php echo base_url(); ?>assets/frontend/img/blog/blog-image-2.jpg" alt="">
+                                    </div>
+                                </div>
+                                <div>
+                                    <div class="img-thumbnail">
+                                        <img class="img-responsive" src="<?php echo base_url(); ?>assets/frontend/img/blog/blog-image-3.jpg" alt="">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
-								<article class="post post-large">
-									<div class="post-image">
-										<div class="owl-carousel owl-theme" data-plugin-options='{"items":1}'>
-											<div>
-												<div class="img-thumbnail">
-													<img class="img-responsive" src="<?php echo base_url();?>assets/frontend/img/blog/blog-image-2.jpg" alt="">
-												</div>
-											</div>
-											<div>
-												<div class="img-thumbnail">
-													<img class="img-responsive" src="<?php echo base_url();?>assets/frontend/img/blog/blog-image-3.jpg" alt="">
-												</div>
-											</div>
-										</div>
-									</div>
+                        <div class="post-date">
+                            <span class="day">10</span>
+                            <span class="month">Jan</span>
+                        </div>
 
-									<div class="post-date">
-										<span class="day">10</span>
-										<span class="month">Jan</span>
-									</div>
+                        <div class="post-content">
 
-									<div class="post-content">
+                            <h2><a href="blog-post.html">Class aptent taciti sociosqu ad litora torquent</a></h2>
+                            <p>Euismod atras vulputate iltricies etri elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla nunc dui, tristique in semper vel, congue sed ligula. Nam dolor ligula, faucibus id sodales in, auctor fringilla libero. Pellentesque pellentesque tempor tellus eget hendrerit. Morbi id aliquam ligula. Aliquam id dui sem. Proin rhoncus consequat nisl, eu ornare mauris tincidunt vitae. [...]</p>
 
-										<h2><a href="blog-post.html">Class aptent taciti sociosqu ad litora torquent</a></h2>
-										<p>Euismod atras vulputate iltricies etri elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla nunc dui, tristique in semper vel, congue sed ligula. Nam dolor ligula, faucibus id sodales in, auctor fringilla libero. Pellentesque pellentesque tempor tellus eget hendrerit. Morbi id aliquam ligula. Aliquam id dui sem. Proin rhoncus consequat nisl, eu ornare mauris tincidunt vitae. [...]</p>
+                            <div class="post-meta">
+                                <span><i class="fa fa-user"></i> By <a href="#">John Doe</a> </span>
+                                <span><i class="fa fa-tag"></i> <a href="#">Duis</a>, <a href="#">News</a> </span>
+                                <span><i class="fa fa-comments"></i> <a href="#">12 Comments</a></span>
+                                <a href="<?php echo base_url(); ?>blog-details" class="btn btn-xs btn-primary pull-right">Read more...</a>
+                            </div>
 
-										<div class="post-meta">
-											<span><i class="fa fa-user"></i> By <a href="#">John Doe</a> </span>
-											<span><i class="fa fa-tag"></i> <a href="#">Duis</a>, <a href="#">News</a> </span>
-											<span><i class="fa fa-comments"></i> <a href="#">12 Comments</a></span>
-											<a href="<?php echo base_url();?>blog-details" class="btn btn-xs btn-primary pull-right">Read more...</a>
-										</div>
+                        </div>
+                    </article>
 
-									</div>
-								</article>
+                    <article class="post post-large">
+                        <div class="post-image">
+                            <div class="owl-carousel owl-theme" data-plugin-options='{"items":1}'>
+                                <div>
+                                    <div class="img-thumbnail">
+                                        <img class="img-responsive" src="<?php echo base_url(); ?>assets/frontend/img/blog/blog-image-3.jpg" alt="">
+                                    </div>
+                                </div>
+                                <div>
+                                    <div class="img-thumbnail">
+                                        <img class="img-responsive" src="<?php echo base_url(); ?>assets/frontend/img/blog/blog-image-2.jpg" alt="">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
-								<article class="post post-large">
-									<div class="post-image">
-										<div class="owl-carousel owl-theme" data-plugin-options='{"items":1}'>
-											<div>
-												<div class="img-thumbnail">
-													<img class="img-responsive" src="<?php echo base_url();?>assets/frontend/img/blog/blog-image-3.jpg" alt="">
-												</div>
-											</div>
-											<div>
-												<div class="img-thumbnail">
-													<img class="img-responsive" src="<?php echo base_url();?>assets/frontend/img/blog/blog-image-2.jpg" alt="">
-												</div>
-											</div>
-										</div>
-									</div>
+                        <div class="post-date">
+                            <span class="day">10</span>
+                            <span class="month">Jan</span>
+                        </div>
 
-									<div class="post-date">
-										<span class="day">10</span>
-										<span class="month">Jan</span>
-									</div>
+                        <div class="post-content">
 
-									<div class="post-content">
+                            <h2><a href="blog-post.html">Class aptent taciti sociosqu ad litora torquent</a></h2>
+                            <p>Euismod atras vulputate iltricies etri elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla nunc dui, tristique in semper vel, congue sed ligula. Nam dolor ligula, faucibus id sodales in, auctor fringilla libero. Pellentesque pellentesque tempor tellus eget hendrerit. Morbi id aliquam ligula. Aliquam id dui sem. Proin rhoncus consequat nisl, eu ornare mauris tincidunt vitae. [...]</p>
 
-										<h2><a href="blog-post.html">Class aptent taciti sociosqu ad litora torquent</a></h2>
-										<p>Euismod atras vulputate iltricies etri elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla nunc dui, tristique in semper vel, congue sed ligula. Nam dolor ligula, faucibus id sodales in, auctor fringilla libero. Pellentesque pellentesque tempor tellus eget hendrerit. Morbi id aliquam ligula. Aliquam id dui sem. Proin rhoncus consequat nisl, eu ornare mauris tincidunt vitae. [...]</p>
+                            <div class="post-meta">
+                                <span><i class="fa fa-user"></i> By <a href="#">John Doe</a> </span>
+                                <span><i class="fa fa-tag"></i> <a href="#">Duis</a>, <a href="#">News</a> </span>
+                                <span><i class="fa fa-comments"></i> <a href="#">12 Comments</a></span>
+                                <a href="<?php echo base_url(); ?>blog-details" class="btn btn-xs btn-primary pull-right">Read more...</a>
+                            </div>
 
-										<div class="post-meta">
-											<span><i class="fa fa-user"></i> By <a href="#">John Doe</a> </span>
-											<span><i class="fa fa-tag"></i> <a href="#">Duis</a>, <a href="#">News</a> </span>
-											<span><i class="fa fa-comments"></i> <a href="#">12 Comments</a></span>
-											<a href="<?php echo base_url();?>blog-details" class="btn btn-xs btn-primary pull-right">Read more...</a>
-										</div>
+                        </div>
+                    </article>-->
 
-									</div>
-								</article>
+<!--                    <ul class="pagination pagination-lg pull-right">
+                        <li><a href="#">«</a></li>
+                        <li class="active"><a href="#">1</a></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                        <li><a href="#">»</a></li>
+                    </ul>-->
 
-								<ul class="pagination pagination-lg pull-right">
-									<li><a href="#">«</a></li>
-									<li class="active"><a href="#">1</a></li>
-									<li><a href="#">2</a></li>
-									<li><a href="#">3</a></li>
-									<li><a href="#">»</a></li>
-								</ul>
+                </div>
+            </div>
 
-							</div>
-						</div>
+        </div>
 
-					</div>
+    </div>
 
-				</div>
-
-			</div>
+</div>
